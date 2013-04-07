8f9818c174e74311e87dda1c457f17bcd1e15b4b
Jove Yu <yushijun110@gmail.com>
2013-04-07 22:09:52 +0800

    fix an re error

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from blog.feeds import LatestPostFeed
 
 urlpatterns = patterns('blog.views',
     url(r'^$','index' ,name='blog_index'),
-    url(r'^post/(?P<postid>\d+)/(?P<postname>\w+)/', 'show_post', name='blog_post'),
+    url(r'^post/(?P<postid>\d+)/(?P<postname>[-\w]+)/', 'show_post', name='blog_post'),
     url(r'^tag/(?P<tagname>.+)/$', 'show_tag', name='blog_tag'),
     url(r'^archives/$', 'archives', name='blog_archives'),
     url(r'^feed/$', LatestPostFeed(), name='blog_feed'),

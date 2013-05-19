a593e019d6849e434955ea9f3367d753acd5651a
Jove Yu <yushijun110@gmail.com>
2013-05-19 17:25:06 +0800

    remove jquery

---
+++ blog/static/jove/js/other.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    var o=document.getElementById("s");
+    var t="其实搜索很简单^_^"
+    o.onblur = function(){
+        if(o.value==""){
+            o.value=t;
+        }
+    }
+    o.onfocus = function(){
+        if(o.value==t){
+            o.value="";
+        }
+    }
+    o.onblur();
+}

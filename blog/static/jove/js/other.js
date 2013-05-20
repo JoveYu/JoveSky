fbd174dd8d37f96d70a7ba618536fd16eeb51c57
Jove Yu <yushijun110@gmail.com>
2013-05-20 10:49:21 +0800

    fix some error

--- blog/static/jove/js/other.js
+++ blog/static/jove/js/other.js
@@ -1,6 +1,6 @@
 window.onload = function(){
     var o=document.getElementById("s");
-    var t="其实搜索很简单^_^"
+    var t="其实搜索很简单^_^";
     o.onblur = function(){
         if(o.value==""){
             o.value=t;

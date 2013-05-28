5795aff24c22c53c31db7a4a51a42d362d8af874
Jove Yu <yushijun110@gmail.com>
2013-05-28 12:17:57 +0800

    add auto thumb

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,14 @@ Email: yushijun110@gmail.com
 Github: https://github.com/JoveYu
 Description: models for blog app
 """
-
+import os
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import permalink
+from django.core.files.base import ContentFile
+from django.conf import settings
+from PIL import Image as PImage
+from cStringIO import StringIO
 
 class Image(models.Model):
     '''图片'''
@@ -17,6 +21,36 @@ class Image(models.Model):
     title = models.CharField(max_length=100, unique=True, verbose_name=u'名称')
     image = models.ImageField(upload_to=UPLOAD_ROOT, verbose_name=u'图片')
 
+    #自动缩略
+    def save(self, *args, **kwargs):
+        org_image = PImage.open(self.image)
+        
+        if org_image.mode not in ('L', 'RGB'):
+            org_image = org_image.convert('RGB')
+        
+        size=getattr(settings,'IMAGE_WIDTH',580)
+        width, height = org_image.size
+        if width > size:
+            delta = width / size
+            height = int(height / delta)
+            org_image.thumbnail((size, height), PImage.ANTIALIAS)
+        
+        #获取文件格式   
+        split = self.image.name.rsplit('.',1)
+        format=split[1]
+        if format.upper()=='JPG':
+            format = 'JPEG'
+            
+        # 将图片存入内存
+        temp_handle = StringIO()
+        org_image.save(temp_handle, format)
+        temp_handle.seek(0) # rewind the file
+        
+        # 保存图像
+        self.image.save(self.image.name, ContentFile(temp_handle.getvalue()) , save=False)
+        
+        super(Image, self).save(*args, **kwargs)
+            
     def __unicode__(self):
         return self.title
 
